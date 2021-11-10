1056e88ecd442a8f093dac9167b9b7f8757a7217
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-11-10 14:22:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+   
+   int n = 0, somma = 0;
+   
+   cin >> n;
+   
+   if(n >= 2)
+   {
+      do{
+         somma = somma + n;
+         
+         cout << somma << endl;
+      }(somma != 0)
+   }
+   else
+   {
+      cout << "errore";
+   }
+      
    return 0;
 }
