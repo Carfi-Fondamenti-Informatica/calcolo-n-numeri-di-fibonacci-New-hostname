7ccf6d239e79a74fac5502d4323265c3cc8ccb64
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-11-10 15:42:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@ int main() {
    
    int n = 0, primo = 1, secondo = 1, somma = 0, i = 0;
    
-   cin >> n;
+   do{
+	   cin >> n;
+   } while(n < 2);
    
    if(n >= 2)
    {
       cout << primo << endl;
       cout << secondo << endl;
    
-      for(i = 0; i < 4; i++)
+      for(i = 0; i < n; i++)
       {
          somma = primo + secondo;
          primo = secondo;
